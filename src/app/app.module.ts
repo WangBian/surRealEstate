8ec0e0c2a69ead08a2a961c884471a3596395fc3
Wang Bian <wang.bian.37@gmail.com>
2019-12-10 21:14:17 -0500

    add nav bar and tools component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ToolsComponent } from './tools/tools.component';
+import { RentalPropertyCalcComponent } from './tools/rental-property-calc/rental-property-calc.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ToolsComponent,
+    RentalPropertyCalcComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tools/rental-property-calc/rental-property-calc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rental-property-calc',
+  templateUrl: './rental-property-calc.component.html',
+  styleUrls: ['./rental-property-calc.component.css']
+})
+export class RentalPropertyCalcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
