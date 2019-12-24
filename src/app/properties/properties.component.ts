a4744b2a036f0bad0ae349b962b269c69fc9f5fd
Wang Bian <wang.bian.37@gmail.com>
2019-12-23 21:16:00 -0500

    improve properties page: add sort function

--- src/app/properties/properties.component.ts
+++ src/app/properties/properties.component.ts
@@ -16,4 +16,20 @@ export class PropertiesComponent implements OnInit {
     console.log(properties);
   }
 
+  sort(order: string) {
+    if (order == 'lowToHigh') {
+      this.properties = this.properties.sort((a, b) => {
+        if (a.price > b.price) { return 1; } else {
+          return -1;
+        }
+      })
+    }
+    if (order == 'highToLow') {
+      this.properties = this.properties.sort((a, b) => {
+        if (a.price > b.price) { return -1; } else {
+          return 1;
+        }
+      })
+    }
+  }
 }
