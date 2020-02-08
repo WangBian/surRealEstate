57349344bcad7ea2875d6460aed499e3cb0d4bed
Wang Bian <wang.bian.37@gmail.com>
2020-02-08 13:28:09 -0500

    update server file

--- server.js
+++ server.js
@@ -13,13 +13,14 @@ app.use(bodyParser.json());
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/surRealEstate";
 app.use(express.static(distDir));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(process.cwd() + '/dist/surRealEstate/index.html'));
+});
 
 /*
         // Serve only the static files form the dist directory
         app.use(express.static(process.cwd() + '/dist/surRealEstate'));
-        app.get('/*', function (req, res) {
-            res.sendFile(path.join(process.cwd() + '/dist/surRealEstate/index.html'));
-        });
+        
 */
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
